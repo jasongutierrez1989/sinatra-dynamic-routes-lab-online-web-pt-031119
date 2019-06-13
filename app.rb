751fac6e4657bcc4720aae4e955942fcdc0743bd
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-13 06:06:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,13 +30,13 @@ class App < Sinatra::Base
     @number2 = params[:number2].to_i
     case @operation
     when 'add'
-      "#{@num1 + @num2}"
+      "#{@num1 + @num2}.to_s"
     when 'multiply'
-      "#{@num1 * @num2}"
+      "#{@num1 * @num2}.to_s"
     when 'subtract'
-      "#{@num1 - @num2}"
+      "#{@num1 - @num2}.to_s"
     when 'divide'
-      "#{@num1 / @num2}"
+      "#{@num1 / @num2}.to_s"
     end
   end
 end
